5e510c53f9fb11518a59f037e0d01c25c7ed8932
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-23 17:01:45 -0600

    Implemented a remove expense item button

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses.action';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt, removeExpense }) => (
   <div>
     <h3>{description}</h3>
     <p>
       {amount} - {createdAt}
     </p>
+    <button onClick={() => removeExpense({ id })}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+const mapDispatchToProps = {
+  removeExpense
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(ExpenseListItem);
