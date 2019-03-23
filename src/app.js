6f508127aee9598b8588a1b7250cdc1104e898ff
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-23 17:17:46 -0600

    Added sortBy select funtionality to UI

--- src/app.js
+++ src/app.js
@@ -6,15 +6,18 @@ import AppRouter from './routes/AppRouter';
 import configureStore from './store/configureStore';
 import './styles/styles.scss';
 import { addExpense } from './actions/expenses.action';
-import { setTextFilter } from './actions/filters.action';
+import { sortByAmount, sortByDate } from './actions/filters.action';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+
+store.dispatch(sortByAmount());
 
 setTimeout(() => {
-  store.dispatch(setTextFilter('gas'));
+  store.dispatch(sortByDate());
 }, 3000);
 
 const App = () => (
--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters.action';
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters.action';
 
 const ExpenseListFilters = props => {
   return (
     <div>
       <input value={props.text} type="text" onChange={e => props.setTextFilter(e.target.value)} />
+      <select
+        value={props.sortBy}
+        onChange={e => (e.target.value === 'date' ? props.sortByDate() : props.sortByAmount())}
+      >
+        <option value="date">Date</option>
+        <option value="amount">Amount</option>
+      </select>
     </div>
   );
 };
 
 const mapStateToProps = state => ({
+  sortBy: state.filters.sortBy,
   text: state.filters.text
 });
 
 const mapDispatchToProps = {
-  setTextFilter
+  setTextFilter,
+  sortByAmount,
+  sortByDate
 };
 
 export default connect(
