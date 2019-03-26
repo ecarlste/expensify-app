f1e3f9e2966d981c518b3aab0796e0f0ebe2abac
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-25 21:03:59 -0600

    Added public route wrapper / removed help page

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -4,21 +4,20 @@ import { Route, Router, Switch } from 'react-router-dom';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import HelpPage from '../components/HelpPage';
 import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <Switch>
-      <Route path="/" exact component={LoginPage} />
+      <PublicRoute path="/" exact component={LoginPage} />
       <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
       <PrivateRoute path="/create" component={AddExpensePage} />
       <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
       <Route component={NotFoundPage} />
     </Switch>
   </Router>
