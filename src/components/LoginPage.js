0c4d336d3b91258a34a19d283fae2b0812efb893
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-25 22:56:02 -0600

    Styled the login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,7 +3,17 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth.action';
 
 export const LoginPage = props => {
-  return <button onClick={props.startLogin}>Login</button>;
+  return (
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">Expensify</h1>
+        <p>It's time to get your expenses under control.</p>
+        <button className="button" onClick={props.startLogin}>
+          Login with Google
+        </button>
+      </div>
+    </div>
+  );
 };
 
 const mapDispatchToProps = {
