a5e4f2415aa8c0138f9ad76d2a2da0b57c9d2804
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-25 20:55:30 -0600

    Added private route component wrapper HOC

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,24 +1,23 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
-import LoginPage from '../components/LoginPage';
-import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
+import LoginPage from '../components/LoginPage';
 import NotFoundPage from '../components/NotFoundPage';
-import { createBrowserHistory } from 'history';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
-    <Header />
     <Switch>
       <Route path="/" exact component={LoginPage} />
-      <Route path="/dashboard" component={ExpenseDashboardPage} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit/:id" component={EditExpensePage} />
+      <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+      <PrivateRoute path="/create" component={AddExpensePage} />
+      <PrivateRoute path="/edit/:id" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
       <Route component={NotFoundPage} />
     </Switch>
